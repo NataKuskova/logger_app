67c5a427f57425eac72fa6ffe0172c094b5ce7e0
Natali <natasha.kuskova@gmail.com>
2016-08-11 02:07:33 +0300

    Delete comments

--- log_app/views.py
+++ log_app/views.py
@@ -40,43 +40,11 @@ def logout_user(request):
     return HttpResponseRedirect('/')
 
 
-# def register_user(request):
-#     if request.method == 'POST':
-#         User.objects.create_user(username=request.POST['username'],
-#                                  password=request.POST['password2'])
-#         return HttpResponseRedirect('/login/')
-#     else:
-#         my_form = UserCreationForm
-#         return render(request, 'register.html', {'form': my_form})
-
-
 class RegisterFormView(FormView):
     form_class = UserCreationForm
-    # form_class = RegistrForm
     success_url = "/login/"
     template_name = "register.html"
 
     def form_valid(self, form):
         self.object = form.save()
         return super(RegisterFormView, self).form_valid(form)
-
-# class IndexView(TemplateView):
-#     template_name = "index.html"
-# #     # logger.error('Something went wrong!')
-
-
-# class LoginFormView(FormView):
-#     form_class = AuthenticationForm
-#     template_name = "login.html"
-#     success_url = "/"
-#
-#     def form_valid(self, form):
-#         self.user = form.get_user()
-#         login(self.request, self.user)
-#         return super(LoginFormView, self).form_valid(form)
-#
-#
-# class LogoutView(View):
-#     def get(self, request):
-#         logout(request)
-#         return HttpResponseRedirect("/")
